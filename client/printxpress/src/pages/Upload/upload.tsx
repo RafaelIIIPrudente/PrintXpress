e1cf48fdd5a15d6dfd3752f5c4980d5c87fe5e0f
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-05-04 17:16:04 +0800

    added pdf icon

--- client/printxpress/src/pages/Upload/upload.tsx
+++ client/printxpress/src/pages/Upload/upload.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar";
 import backgroundImage from "../../assets/bg.png";
+import pdf from "../../assets/pdf.png";
 import qr from "../../assets/qr.png";
 
 const Upload = () => {
@@ -10,14 +11,21 @@ const Upload = () => {
     >
       <div className="max-w-[1500px] mx-auto text-center">
         <Navbar />
-        <div className="flex items-center justify-center mt-16 px-12">
+        <div className="flex items-center justify-center mt-16 px-12 relative">
           <div className="mr-16">
             <img src={qr} alt="qr" className="w-80 h-80" />
-            <div className="text-2xl font-semibold"> Scan Me </div>
-            <div className="text-2xl font-light">or log in to 192.168.1.1</div>
+            <div className="text-2xl font-semibold mt-2"> Scan Me </div>
+            <div className="text-2xl font-light mt-1">
+              or log in to 192.168.1.1
+            </div>
           </div>
-
-          <div className="border rounded-lg w-[900px] h-[500px] bg-white"></div>
+          <div className="border rounded-lg w-[900px] h-[500px] bg-white relative">
+            <img
+              src={pdf}
+              alt="pdf"
+              className="absolute top-0 right-0 -mt-10 mr-4 w-20 h-20"
+            />
+          </div>
         </div>
       </div>
     </div>
