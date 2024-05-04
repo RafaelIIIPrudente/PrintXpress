7933ba7921a367bfd3dd5c8ed9c27426d784129e
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-05-04 16:05:01 +0800

    added homepage

--- client/printxpress/src/main.tsx
+++ client/printxpress/src/main.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import "./index.css";
 
-// Import the FileUpload component
-import FileUpload from './pages/Upload/FileUpload'
-import Print from './pages/Print/Print';
-import Print2 from './pages/Print/Print2';
+import FileUpload from "./pages/Upload/FileUpload";
+import Print from "./pages/Print/Print";
+import Print2 from "./pages/Print/Print2";
+import Home from "./pages/Home/home";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/upload" element={<FileUpload />} />
         <Route path="/print" element={<Print />} />
         <Route path="/print2" element={<Print2 />} />
-
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
